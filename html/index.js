d911363d7824c41a901874d1b199e5ea06308897
dav1taa <nebieridze.dato12@gmail.com>
2022-11-26 13:25:13 +0400

    ragaceebi

---
+++ html/index.js
@@ -0,0 +1,46 @@
+// const promiseToDoSomething = () => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve ("i did something"),1000);
+//     })
+// }
+
+// const watchOverSomeoneDoingSomething = async () => {
+//     const something = await promiseToDoSomething()
+//     return something + 'and I watched'
+//    }
+   
+//    const watchOverSomeoneWatchingSomeoneDoingSomething = async () => {
+//     const something = await watchOverSomeoneDoingSomething()
+//     return something + 'and I watched as well'
+//    }
+   
+//    watchOverSomeoneWatchingSomeoneDoingSomething().then(res => {
+//     console.log(res)
+//    })
+
+
+
+// var string = "welcome to this javasvafas gudide!";
+
+// var reverseEntireSentence = reverseBySeparator (string, "");
+
+// var reverseEachWord = reverseBySeparator (reverseEntireSentence, "");
+
+// function reverseBySeparator(string, separator) {
+//     return string.split(separator).reverse().join(separator);
+    
+// }
+// console.log(string)
+
+
+// function duplicate(arr) {
+//       console.log(arr.concat(arr));
+//     }
+//     duplicate([1, 2, 3, 4, 5]);
+
+// (function () {
+//     var a=b=3;   
+// }) ()
+
+// console.log ("a defined?" + (typeof a !== "undefined")) ;
+// console.log("b defined? " + (typeof b !== "undefined"));
